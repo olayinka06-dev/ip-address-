7758cefc13e9e15f52ee4aca909d2cadddc799d7
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-16 08:01:19 -0700

    Creating the Search Engine Page

--- src/components/searchengine/SearchEngine.jsx
+++ src/components/searchengine/SearchEngine.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
-import {IoIosArrowForward} from 'react-icons/io'
+import {IoIosArrowForward} from 'react-icons/io';
+
 
 
 const SearchEngine = () => {
   const [ipAddress, setIpAddress] = useState("");
+  const [ipInfo, setIpInfo] = useState("");
+  const [error, setError] = useState("");
   const handleSubmit = async (e) => {
+
+    if (!ipAddress) {
+      setError("Please enter an Your Ip Address");
+      return;
+    }
 
     try {
       const IP_KEY = "at_iPdZbBjjZFpxMAg5R5gs3ZY4uKbkv";
-      const IP_URL = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${IP_KEY}&ipAddress=${ipAddress}`
-    } catch (error) {
+      const IP_URL = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${IP_KEY}&ipAddress=${ipAddress}`;
+      const response = await axios.get(IP_URL);
+
+      setIpInfo(response.data);
+      setError("")
       
+    } catch (error) {
+      if (error.request) {
+        setError("Ip Address not Found")
+      }
+      else if (condition) {
+        setError("Network Error, Please Check your Internet Connection")
+      }
+      else{
+        setError("Something Went Wrong Unexpectedly")
+      }
     }
 
   }
